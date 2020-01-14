4b9a4dabd2cd8e7dc1455b7ade62a3ff952b1ed1
Ong <Ong@gmail.com>
2020-01-14 13:49:08 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import allReducer from './reducers'
+import { Provider } from 'react-redux';
+
+//STORE -> GLOBALIZED STATE
+const myStore = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+
+
+ReactDOM.render(
+  <Provider store = {myStore}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
